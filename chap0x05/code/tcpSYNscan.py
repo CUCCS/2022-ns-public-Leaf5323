453666d6867dfc63a85e223ef7a47bc4c05199d0
viewtifulLeaf <viewtifulleaf@qq.com>
2022-10-16 23:32:19 +0800

    紧急修复构建TCP包中端口以str类传入导致出现的Error sending packets错误😃

--- chap0x05/code/tcpSYNscan.py
+++ chap0x05/code/tcpSYNscan.py
@@ -3,27 +3,28 @@ import scapy.sendrecv as ss
 from sys import argv
 
 def createSYNpacket(destinationIP,destinationPort):
-    packet=sli.IP(dst=destinationIP)/sli.TCP(dport=destinationPort,flags="S")
+    packet=sli.IP(dst=destinationIP)/sli.TCP(dport=int(destinationPort),flags="S")
     return packet
 
 def createRSTpacket(destinationIP,destinationPort):
-    packet=sli.IP(dst=destinationIP)/sli.TCP(dport=destinationPort,flags="R")
+    packet=sli.IP(dst=destinationIP)/sli.TCP(dport=int(destinationPort),flags="R")
     return packet
 
 def tcpSYNscan(targetIP,targetPort):
     SYNpacket=createSYNpacket(destinationIP=targetIP,destinationPort=targetPort)
     gotAnswer=ss.sr1(SYNpacket,timeout=10)
     if not gotAnswer:
-        print("Port:"+targetPort+" Filtered")
+        print("Port "+targetPort+": Filtered")
     elif gotAnswer.getlayer(sli.TCP).flags=='RA':
-        print("Port:"+targetPort+" Closed")
+        print("Port "+targetPort+": Closed")
     elif gotAnswer.getlayer(sli.TCP).flags=='SA':
         RSTpacket=createRSTpacket(targetIP,targetPort)
         ss.send(RSTpacket)
-        print("Port:"+targetPort+" Open")
+        print("Port "+targetPort+": Open")
 
 if __name__=='__main__':
     if len(argv)<2:
         print("Usage: python tcpSYNscan.py [target IP] [target port]")
     else:
-        tcpSYNscan(targetIP=argv[0], targetPort=argv[1])
+        targetIP,targetPort=argv[1],argv[2]
+        tcpSYNscan(targetIP,targetPort)
